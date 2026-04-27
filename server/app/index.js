3622d3dc5796003c12270ad1b2a38671dcfa5f42
Alena <nikishova-alena@mail.ru>
2026-04-28 00:38:41 +0300

    server - уберет dotenv в корень запуска сервера

--- server/app/index.js
+++ server/app/index.js
@@ -1,16 +1,18 @@
-
 import express from 'express'
 import {db} from "../db/connect.js";
+import dotenv from "dotenv";
 
 const app = express()
 const port = 3000
 
+dotenv.config();
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-//проверка подключения
+
 const result = await db.query('SELECT 1')
-console.log(result)
+console.log(result.rows[0])
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- server/db/connect.js
+++ server/db/connect.js
@@ -1,6 +1,4 @@
 import {Pool} from "pg";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const db = new Pool({
     host: process.env.PGHOST,
