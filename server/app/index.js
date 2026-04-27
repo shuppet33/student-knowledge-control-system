64588a7a5550c7fa2c3caa32991a7a58f2be6bae
Ntoils <rredqwerty@gmail.com>
2026-04-27 23:30:23 +0300

    server - добавит подключение к базе данных

--- server/app/index.js
+++ server/app/index.js
@@ -1,10 +1,16 @@
-const express = require('express')
+
+import express from 'express'
+import {db} from "../db/connect.js";
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+//проверка подключения
+const result = await db.query('SELECT 1')
+console.log(result)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- server/db/connect.js
+++ server/db/connect.js
@@ -1,6 +1,11 @@
 import {Pool} from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
-
-const db = new Pool({
-    // Добавить подключение к БД
+export const db = new Pool({
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT,
 })
