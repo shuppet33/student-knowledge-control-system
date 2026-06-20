e799af046d9597efb770caffe3ea86d25c36cd8f
Alena <nikishova-alena@mail.ru>
2026-06-20 19:11:54 +0300

    [refactor] - обновит форматирование состояния просмотра вопросов

--- client/src/modules/teachers/ui/question-preview/question-preview.state.ts
+++ client/src/modules/teachers/ui/question-preview/question-preview.state.ts
@@ -2,14 +2,9 @@ import { action, atom } from '@reatom/framework'
 
 import type { Question } from './question-preview.types'
 
-export const isQuestionPreviewOpenAtom = atom(
-    false,
-    'isQuestionPreviewOpenAtom',
-)
-export const selectedQuestionIdAtom = atom(
-    null as number | null,
-    'selectedQuestionIdAtom',
-)
+export const isQuestionPreviewOpenAtom = atom(false, 'isQuestionPreviewOpenAtom')
+export const selectedQuestionIdAtom = atom(null as number | null, 'selectedQuestionIdAtom')
+
 export const openQuestionPreviewAction = action((ctx, questionId: number) => {
     selectedQuestionIdAtom(ctx, questionId)
     isQuestionPreviewOpenAtom(ctx, true)
@@ -20,34 +15,24 @@ export const closeQuestionPreviewAction = action((ctx) => {
     selectedQuestionIdAtom(ctx, null)
 }, 'closeQuestionPreviewAction')
 
-export const nextQuestionAction = action(
-    (ctx, questions: Question[]) => {
-        const currentQuestionId = ctx.get(selectedQuestionIdAtom)
-        const currentIndex = questions.findIndex(
-            (question) => question.id === currentQuestionId,
-        )
+export const nextQuestionAction = action((ctx, questions: Question[]) => {
+    const currentQuestionId = ctx.get(selectedQuestionIdAtom)
+    const currentIndex = questions.findIndex((question) => question.id === currentQuestionId)
 
-        if (currentIndex === -1 || currentIndex >= questions.length - 1) {
-            return
-        }
+    if (currentIndex === -1 || currentIndex >= questions.length - 1) {
+        return
+    }
 
-        selectedQuestionIdAtom(ctx, questions[currentIndex + 1].id)
-    },
-    'nextQuestionAction',
-)
+    selectedQuestionIdAtom(ctx, questions[currentIndex + 1].id)
+}, 'nextQuestionAction')
 
-export const prevQuestionAction = action(
-    (ctx, questions: Question[]) => {
-        const currentQuestionId = ctx.get(selectedQuestionIdAtom)
-        const currentIndex = questions.findIndex(
-            (question) => question.id === currentQuestionId,
-        )
+export const prevQuestionAction = action((ctx, questions: Question[]) => {
+    const currentQuestionId = ctx.get(selectedQuestionIdAtom)
+    const currentIndex = questions.findIndex((question) => question.id === currentQuestionId)
 
-        if (currentIndex <= 0) {
-            return
-        }
+    if (currentIndex <= 0) {
+        return
+    }
 
-        selectedQuestionIdAtom(ctx, questions[currentIndex - 1].id)
-    },
-    'prevQuestionAction',
-)
+    selectedQuestionIdAtom(ctx, questions[currentIndex - 1].id)
+}, 'prevQuestionAction')
